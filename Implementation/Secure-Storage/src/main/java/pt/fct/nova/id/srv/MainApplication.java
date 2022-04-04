1c1b720f7373d9855676f0218e89ad1ac32e7b92
diogo <d.cebola@campus.fct.unl.pt>
2022-04-04 14:59:16 +0100

    fixed path error for api

--- Implementation/Secure-Storage/src/main/java/pt/fct/nova/id/srv/MainApplication.java
+++ Implementation/Secure-Storage/src/main/java/pt/fct/nova/id/srv/MainApplication.java
@@ -3,12 +3,10 @@ package pt.fct.nova.id.srv;
 
 import pt.fct.nova.id.srv.Resources.ControlResource;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
-@ApplicationPath("/")
 public class MainApplication extends Application {
     private final Set<Object> singletons = new HashSet<>();
     private final Set<Class<?>> resources = new HashSet<>();
