5fd3a17aeac8e5ed7e1313540d6c51b28ff6507b
diogo <d.cebola@campus.fct.unl.pt>
2022-04-20 19:29:37 +0100

    adding parser

--- Implementation/Secure-Storage/src/main/java/pt/fct/nova/id/srv/presentation/controllers/StorageController.java
+++ Implementation/Secure-Storage/src/main/java/pt/fct/nova/id/srv/presentation/controllers/StorageController.java
@@ -2,7 +2,13 @@ package pt.fct.nova.id.srv.presentation.controllers;
 
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFParser;
 import pt.fct.nova.id.srv.presentation.api.StorageAPI;
+
+import java.io.ByteArrayInputStream;
 
 
 @Path("storage")
@@ -10,6 +16,12 @@ public class StorageController implements StorageAPI {
 
     @Override
     public Response upload() {
+        Model model = ModelFactory.createDefaultModel();
+        model.read(new ByteArrayInputStream(new byte[10]), "lang");
+        for (Triple t : model.getGraph().stream().toList()) {
+
+
+        }
         //TODO: Store Indexes
         //TODO: Store Triples
         return Response.ok("NOT IMPLEMENTED").status(Response.Status.NOT_IMPLEMENTED).build();
