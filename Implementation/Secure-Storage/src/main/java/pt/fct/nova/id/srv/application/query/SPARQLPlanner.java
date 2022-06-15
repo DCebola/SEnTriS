d3b22e1ccf1e81c159f6b25c30dfd35512f0271c
Diogo <d.cebola@campus.fct.unl.pt>
2022-06-15 17:14:07 +0100

    Created function structure for missing job, added TODOs

--- Implementation/Secure-Storage/src/main/java/pt/fct/nova/id/srv/application/query/SPARQLPlanner.java
+++ Implementation/Secure-Storage/src/main/java/pt/fct/nova/id/srv/application/query/SPARQLPlanner.java
@@ -3,6 +3,7 @@ package pt.fct.nova.id.srv.application.query;
 import org.apache.jena.atlas.lib.NotImplemented;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
+import org.apache.jena.sparql.algebra.JoinType;
 import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.algebra.OpVisitorByTypeBase;
 import org.apache.jena.sparql.algebra.OpWalker;
@@ -12,6 +13,7 @@ import org.apache.jena.sparql.engine.binding.Binding;
 import pt.fct.nova.id.srv.application.query.jobs.BindJob;
 import pt.fct.nova.id.srv.application.query.jobs.GetJob;
 import pt.fct.nova.id.srv.application.query.jobs.Job;
+import pt.fct.nova.id.srv.application.query.jobs.ValuesJob;
 
 import java.util.*;
 
@@ -20,7 +22,14 @@ import static pt.fct.nova.id.srv.application.Utils.generateID;
 
 public class SPARQLPlanner extends OpVisitorByTypeBase {
 
-    private final Deque<Job> plan = new LinkedList<>();
+    private final Map<Op, String> parsed_op;
+
+    private final Deque<Job> plan;
+
+    public SPARQLPlanner() {
+        this.parsed_op = new HashMap<>();
+        this.plan = new LinkedList<>();
+    }
 
     void opVisitorWalker(Op op) {
         OpWalker.walk(op, this);
@@ -33,115 +42,160 @@ public class SPARQLPlanner extends OpVisitorByTypeBase {
     @Override
     public void visit0(Op0 op) throws NotImplemented {
         System.out.println("OP0: " + op);
-        List<Triple> triples;
         if (op instanceof OpBGP) {
-            triples = ((OpBGP) op).getPattern().getList();
-            generateGetJobs(triples);
+            generateGetJobs((OpBGP) op);
         } else if (op instanceof OpTriple) {
-            triples = ((OpTriple) op).asBGP().getPattern().getList();
-            generateGetJobs(triples);
+            generateGetJobs(((OpTriple) op).asBGP());
         } else if (op instanceof OpTable) {
-            generateValuesJob(((OpTable) op).getTable().rows());
-        } /* else
-            throw new NotImplemented();
-          */
-    }
-
-    private void generateValuesJob(Iterator<Binding> bindings) {
-
-    }
-
-    private void generateGetJobs(List<Triple> triples) {
-        triples.forEach(
-                t -> {
-                    Node s = t.getSubject();
-                    Node p = t.getPredicate();
-                    Node o = t.getObject();
-                    plan.addFirst(new GetJob(generateID(), extractVariablesPattern(s, p, o), s, p, o));
-                }
-        );
-    }
-
-    @Override
-    public void visit1(Op1 op) {
-        if (op instanceof OpExtendAssign) {
-            System.out.println("OP1 OpExtendAssign: " + op);
-            System.out.println(((OpExtendAssign) op).getVarExprList().getExprs());
-        } else if (op instanceof OpModifier) {
-            visitOpModifier((OpModifier) op);
-            System.out.println("OP1 Modifier: " + op);
-        } else if (op instanceof OpFilter) {
-            System.out.println(((OpFilter) op).getExprs());
-            System.out.println("OP1 OpFilter: " + op);
-        } else if (op instanceof OpGroup) {
-            System.out.println("OP1 OpGroup: " + op);
-            System.out.println(((OpGroup) op).getGroupVars().getExprs());
-            System.out.println(((OpGroup) op).getAggregators());
-        }/* else if (op instanceof OpGraph) {
-            System.out.println("OP1 OpGraph: " + op);
-        } else if (op instanceof OpLabel) {
-            System.out.println("OP1 Label: " + op);
-        } else if (op instanceof OpProcedure) {
-            System.out.println("OP1 OpProcedure: " + op);
-        } else if (op instanceof OpPropFunc) {
-            System.out.println("OP1 OpPropFunc: " + op);
-        } else if (op instanceof OpService) {
-            System.out.println("OP1 OpService: " + op);
+            generateValuesJob((OpTable) op);
+        } /* else if (op instanceof OpDatasetNames) {
+        } else if (op instanceof OpPath) {
+        } else if (op instanceof OpNull) {
+        } else if (op instanceof OpQuad) {
+        } else if (op instanceof OpQuadBlock) {
+        } else if (op instanceof OpQuadPattern) {
         } else
             throw new NotImplemented();
         */
     }
 
-    private void visitOpModifier(OpModifier op) {
-        if (op instanceof OpDistinct) {
-
-        } else if (op instanceof OpReduced) {
-
-        } else if (op instanceof OpOrder) {
-
-        } else if (op instanceof OpProject) {
-
-        } else if (op instanceof OpSlice) {
-
-        } /* else if (op instanceof OpTopN) {
-
-        }  else if (op instanceof OpList) {
-
-        } */
+    private void generateGetJobs(OpBGP op) {
+        op.getPattern().getList().forEach(
+                t -> {
+                    Node s = t.getSubject();
+                    Node p = t.getPredicate();
+                    Node o = t.getObject();
+                    String jobID = parsed_op.get(op);
+                    if (jobID == null) {
+                        jobID = generateID();
+                        parsed_op.put(op, jobID);
+                        plan.addFirst(new GetJob(jobID, extractVariablesPattern(s, p, o), s, p, o));
+                    }
+                }
+        );
     }
 
-    private void generateBindJob(VarExprList exprList) {
-        exprList.getExprs().forEach(
+    private void generateValuesJob(OpTable op) {
+        Set<Binding> values = new LinkedHashSet<>();
+        op.getTable().rows().forEachRemaining(values::add);
+        String jobID = parsed_op.get(op);
+        if (jobID == null) {
+            jobID = generateID();
+            parsed_op.put(op, jobID);
+            plan.addFirst(new ValuesJob(generateID(), values));
+        }
+    }
+
+    @Override
+    public void visit1(Op1 op) {
+        System.out.println("OP1: " + op);
+        if (op instanceof OpExtendAssign) {
+            generateBindJob((OpExtendAssign) op);
+        } else if (op instanceof OpFilter) {
+            generateFilterJob((OpFilter) op);
+        } else if (op instanceof OpGroup) {
+            generateGroupJob((OpGroup) op);
+        } else if (op instanceof OpModifier) {
+            visitOpModifier((OpModifier) op);
+        }/* else if (op instanceof OpGraph) {
+        } else if (op instanceof OpLabel) {
+        } else if (op instanceof OpProcedure) {
+        } else if (op instanceof OpPropFunc) {
+        } else if (op instanceof OpService) {
+        } else
+            throw new NotImplemented();
+        */
+    }
+
+
+    private void generateBindJob(OpExtendAssign op) {
+        op.getVarExprList().getExprs().forEach(
                 (var, expr) -> plan.addFirst(new BindJob(generateID(), var, expr))
         );
     }
 
+    private void generateFilterJob(OpFilter op) {
+        //TODO: Generate FilterJobs
+    }
+
+
+    private void generateGroupJob(OpGroup op) {
+        //TODO: Generate GroupJobs
+    }
+
+
+    private void visitOpModifier(OpModifier op) {
+        if (op instanceof OpDistinctReduced) {
+            generateDistinctJob((OpDistinctReduced) op);
+        } else if (op instanceof OpOrder) {
+            generateOrderByJob((OpOrder) op);
+        } else if (op instanceof OpProject) {
+            generateProjectJob((OpProject) op);
+        } else if (op instanceof OpSlice) {
+            generateSliceJob((OpSlice) op);
+        }/* else if (op instanceof OpTopN) {
+        }  else if (op instanceof OpList) {
+        }  else
+             throw new NotImplemented();
+        */
+    }
+
+    private void generateProjectJob(OpProject op) {
+        //TODO: Generate ProjectJobs
+    }
+
+    private void generateDistinctJob(OpDistinctReduced op) {
+        //TODO: Generate DistinctJobs
+    }
+
+    private void generateOrderByJob(OpOrder op) {
+        //TODO: Generate OrderByJobs
+    }
+
+    private void generateSliceJob(OpSlice op) {
+        //TODO: Generate SliceJobs
+    }
+
+
     @Override
     public void visit2(Op2 op) {
+        System.out.println("OP2: " + op);
         if (op instanceof OpJoin) {
-            System.out.println("OP2: " + op);
+            generateJoinJob((OpJoin) op);
         } else if (op instanceof OpLeftJoin) {
-            System.out.println("OP2: " + op);
+            generateOptionalJob((OpLeftJoin) op);
         } else if (op instanceof OpMinus) {
-            System.out.println("OP2: " + op);
+            generateMinusJob((OpMinus) op);
         } else if (op instanceof OpUnion) {
-            System.out.println("OP2: " + op);
-        } /* else if (op instanceof OpConditional) {
-            System.out.println("OP2: " + op);
+            generateUnionJob((OpUnion) op);
+        }/* else if (op instanceof OpConditional) {
         } else if (op instanceof OpDiff) {
-            System.out.println("OP2: " + op);
         } else
             throw new NotImplemented();
           */
+    }
+
+    private void generateJoinJob(OpJoin op) {
+        //TODO: Generate JoinJob
+    }
+
+    private void generateOptionalJob(OpLeftJoin op) {
+        //TODO: Generate OptionalJob
+    }
+
+    private void generateMinusJob(OpMinus op) {
+        //TODO: Generate MinusJob
+    }
+
+    private void generateUnionJob(OpUnion op) {
+        //TODO: Generate UnionJob
     }
 
     @Override
     public void visitN(OpN op) {
         /*
         if (op instanceof OpDisjunction) {
-            System.out.println("OPN OpDisjunction: " + op);
         } else if (op instanceof OpSequence) {
-            System.out.println("OPN OpSequence: " + op);
         } else
            throw new NotImplemented();
          */
--- Implementation/Secure-Storage/src/main/java/pt/fct/nova/id/srv/application/query/jobs/ValuesJob.java
+++ Implementation/Secure-Storage/src/main/java/pt/fct/nova/id/srv/application/query/jobs/ValuesJob.java
@@ -1,10 +1,22 @@
 package pt.fct.nova.id.srv.application.query.jobs;
 
 
-public class ValuesJob extends Job{
+import org.apache.jena.sparql.engine.binding.Binding;
 
+import java.util.Iterator;
+import java.util.Set;
 
-    public ValuesJob(String jobID) {
+public class ValuesJob extends Job {
+
+    private final Set<Binding> values;
+
+    public ValuesJob(String jobID, Set<Binding> values) {
         super(jobID);
+        this.values = values;
     }
+
+    public Set<Binding> getValues() {
+        return values;
+    }
+
 }
