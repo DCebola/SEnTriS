e5a85a92508fcad0a301a26c5d72fe248708ea11
Diogo <d.cebola@campus.fct.unl.pt>
2022-10-13 16:31:23 +0100

    Updated ssl script

--- SEnTri/Scripts/generate-x509-certificate.sh
+++ SEnTri/Scripts/generate-x509-certificate.sh
@@ -29,11 +29,17 @@ generate_cert() {
 }
 
 openssl req -x509 -newkey rsa:4096 -keyout ./SSL/ca-key.pem -out ./SSL/ca-cert.pem -days 365 -subj "/C=PT/ST=Setubal/L=Almada/O=NOVA.ID.FCT/OU=DI/CN=dcebola" 
-generate_cert server '-ext .SSL/openssl.conf -extensions server_cert'
-generate_cert client '-ext ./SSL/openssl.conf -extensions client_cert'
+generate_cert triplestore '-ext ./SSL/openssl.conf -extensions server_cert'
+generate_cert proxy '-ext .SSL/openssl.conf -extensions server_cert'
+generate_cert data-owner '-ext ./SSL/openssl.conf -extensions client_cert'
 generate_cert redis 
 
 [ -f ./SSL/redis.dh ] || openssl dhparam -out ./SSL/redis.dh 2048
 
-keytool -import -file ./SSL/redis-cert.pem -alias redis -keystore ./SSL/server-truststore.ks
-keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/server-truststore.ks
+keytool -import -file ./SSL/redis-cert.pem -alias redis -keystore ./SSL/triplestore-truststore.ks
+keytool -import -file ./SSL/proxy-cert.pem -alias proxy -keystore ./SSL/triplestore-truststore.ks
+keytool -import -file ./SSL/data-owner-cert.pem -alias data-owner -keystore ./SSL/triplestore-truststore.ks
+keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/triplestore-truststore.ks
+
+keytool -import -file ./SSL/triplestore-cert.pem -alias triplestore -keystore ./SSL/proxy-truststore.ks
+keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/proxy-truststore.ks
