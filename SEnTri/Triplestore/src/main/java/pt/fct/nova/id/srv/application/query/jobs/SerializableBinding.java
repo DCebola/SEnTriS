29ad121cc4616b7a20038ed9f05ae254557244a5
diogo <d.cebola@campus.fct.unl.pt>
2022-12-30 19:36:39 +0000

    Added support for DESCRIBE, ASK, CONSTRUCT

---
+++ SEnTri/Triplestore/src/main/java/pt/fct/nova/id/srv/application/query/jobs/SerializableBinding.java
@@ -0,0 +1,35 @@
+package pt.fct.nova.id.srv.application.query.jobs;
+
+import org.apache.jena.sparql.core.Var;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class SerializableBinding implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 3345654444362467694L;
+    private final Map<Var, String> values;
+
+    public SerializableBinding(Map<Var, String> values) {
+        this.values = values;
+    }
+
+    public SerializableBinding() {
+        this.values = new HashMap<>();
+    }
+
+    public Iterator<Var> vars() {
+        return values.keySet().iterator();
+    }
+
+    public String get(Var var) {
+        return values.get(var);
+    }
+
+    public Map<Var, String> getValues() {
+        return values;
+    }
+}
