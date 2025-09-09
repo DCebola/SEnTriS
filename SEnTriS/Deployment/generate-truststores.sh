999973f0d444dc44aef8dd4be7eb101cb0aeee94
DCebola <diogohecebola@gmail.com>
2025-09-09 19:27:47 +0100

    optimization of redis configs, no timeout errors in lubm-1, v1 bench so should be fixed

--- SEnTriS/Deployment/generate-truststores.sh
+++ SEnTriS/Deployment/generate-truststores.sh
@@ -5,57 +5,57 @@ if [ $# -ne 0  ]; then
 fi
 
 #IAM Provider Truststore
-keytool -import -file ./SSL/iam-provider-redis-cert.pem -alias redis -keystore ./SSL/iam-provider-truststore.ks
-wait
-keytool -import -file ./SSL/client-cert.pem -alias client -keystore ./SSL/iam-provider-truststore.ks
-wait
-keytool -import -file ./SSL/triplestore-cert.pem -alias triplestore -keystore ./SSL/iam-provider-truststore.ks
-wait
-keytool -import -file ./SSL/proxy-cert.pem -alias proxy -keystore ./SSL/iam-provider-truststore.ks
-wait
-keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/triplestore-truststore.ks
+# keytool -import -file ./SSL/iam-provider-redis-cert.pem -alias redis -keystore ./SSL/iam-provider-truststore.ks
+# wait
+# keytool -import -file ./SSL/client-cert.pem -alias client -keystore ./SSL/iam-provider-truststore.ks
+# wait
+# keytool -import -file ./SSL/triplestore-cert.pem -alias triplestore -keystore ./SSL/iam-provider-truststore.ks
+# wait
+# keytool -import -file ./SSL/proxy-cert.pem -alias proxy -keystore ./SSL/iam-provider-truststore.ks
+# wait
+keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/iam-provider-truststore.ks
 wait
 
 #Vault Truststore
-keytool -import -file ./SSL/vault-redis-cert.pem -alias redis -keystore ./SSL/vault-truststore.ks
-wait
-keytool -import -file ./SSL/iam-provider-cert.pem -alias iam-provider -keystore ./SSL/vault-truststore.ks
-wait
-keytool -import -file ./SSL/client-cert.pem -alias client -keystore ./SSL/vault-truststore.ks
-wait
+# keytool -import -file ./SSL/vault-redis-cert.pem -alias redis -keystore ./SSL/vault-truststore.ks
+# wait
+# keytool -import -file ./SSL/iam-provider-cert.pem -alias iam-provider -keystore ./SSL/vault-truststore.ks
+# wait
+# keytool -import -file ./SSL/client-cert.pem -alias client -keystore ./SSL/vault-truststore.ks
+# wait
 keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/vault-truststore.ks
 wait
 
 #Triplestore Truststore
-keytool -import -file ./SSL/triplestore-redis-cert.pem -alias redis -keystore ./SSL/triplestore-truststore.ks
-wait
-keytool -import -file ./SSL/iam-provider-cert.pem -alias iam-provider -keystore ./SSL/triplestore-truststore.ks
-wait
-keytool -import -file ./SSL/proxy-cert.pem -alias proxy -keystore ./SSL/triplestore-truststore.ks
-wait
-keytool -import -file ./SSL/client-cert.pem -alias client -keystore ./SSL/triplestore-truststore.ks
-wait
+# keytool -import -file ./SSL/triplestore-redis-cert.pem -alias redis -keystore ./SSL/triplestore-truststore.ks
+# wait
+# keytool -import -file ./SSL/iam-provider-cert.pem -alias iam-provider -keystore ./SSL/triplestore-truststore.ks
+# wait
+# keytool -import -file ./SSL/proxy-cert.pem -alias proxy -keystore ./SSL/triplestore-truststore.ks
+# wait
+# keytool -import -file ./SSL/client-cert.pem -alias client -keystore ./SSL/triplestore-truststore.ks
+# wait
 keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/triplestore-truststore.ks
 wait
 
 #Proxy Truststore
-keytool -import -file ./SSL/proxy-redis-cert.pem -alias redis -keystore ./SSL/proxy-truststore.ks
-wait
-keytool -import -file ./SSL/iam-provider-cert.pem -alias iam-provider -keystore ./SSL/proxy-truststore.ks
-wait
-keytool -import -file ./SSL/client-cert.pem -alias client -keystore ./SSL/proxy-truststore.ks
-wait
-keytool -import -file ./SSL/triplestore-cert.pem -alias triplestore -keystore ./SSL/proxy-truststore.ks
-wait
+# keytool -import -file ./SSL/proxy-redis-cert.pem -alias redis -keystore ./SSL/proxy-truststore.ks
+# wait
+# keytool -import -file ./SSL/iam-provider-cert.pem -alias iam-provider -keystore ./SSL/proxy-truststore.ks
+# wait
+# keytool -import -file ./SSL/client-cert.pem -alias client -keystore ./SSL/proxy-truststore.ks
+# wait
+# keytool -import -file ./SSL/triplestore-cert.pem -alias triplestore -keystore ./SSL/proxy-truststore.ks
+# wait
 keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/proxy-truststore.ks
 wait
 
 #Client Truststore
-keytool -import -file ./SSL/iam-provider-cert.pem -alias iam-provider -keystore ./SSL/client-truststore.ks
-wait
-keytool -import -file ./SSL/triplestore-cert.pem -alias triplestore -keystore ./SSL/client-truststore.ks
-wait
-keytool -import -file ./SSL/proxy-cert.pem -alias proxy -keystore ./SSL/client-truststore.ks
-wait
+# keytool -import -file ./SSL/iam-provider-cert.pem -alias iam-provider -keystore ./SSL/client-truststore.ks
+# wait
+# keytool -import -file ./SSL/triplestore-cert.pem -alias triplestore -keystore ./SSL/client-truststore.ks
+# wait
+# keytool -import -file ./SSL/proxy-cert.pem -alias proxy -keystore ./SSL/client-truststore.ks
+# wait
 keytool -import -file ./SSL/ca-cert.pem -alias ca -keystore ./SSL/client-truststore.ks
 wait
