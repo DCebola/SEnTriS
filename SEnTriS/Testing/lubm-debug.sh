792ce4ac7aed39afd230412e9715c1ce1b32e921
DCebola <diogohecebola@gmail.com>
2025-09-21 01:14:29 +0100

    .

--- SEnTriS/Testing/lubm-debug.sh
+++ SEnTriS/Testing/lubm-debug.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ $# -ne 2  ]; then
-    echo "Usage: lubm-benchmark <docker-registry> <experiment-id>"
+    echo "Usage: lubm-debug <docker-registry> <experiment-id>"
     exit 1
 fi
 
 if [ "$(curl -k -s -o /dev/null -w "%{http_code}" -X POST https://localhost:8091/IAMProvider/api/ctrl/init)" = "200" ]; then
-  for dataset in lubm-0 lubm-1 lubm-2 lubm-3 lubm-4 lubm-5
+  for dataset in lubm-1
   do
     for version in v2
     do
-      for scenario in lubm-upload lubm-query
+      for scenario in lubm-debug
       do
         echo " > $scenario (dataset=$dataset, version=$version)"
         docker run --rm \
@@ -18,10 +18,10 @@ if [ "$(curl -k -s -o /dev/null -w "%{http_code}" -X POST https://localhost:8091
           -v "$PWD/results/$2:/tests/$scenario.json" \
           "$1/lubm-test-runner" \
           --insecure \
-          --variables "{ \"dataset\": [\"$dataset\"], \"version\": [\"$version\"] }" \
+          --variables "{ \"triplestoreID\": [\"test-$version-$dataset\"], \"version\": [\"$version\"], \"dataset\": [\"$dataset\"], \"username\": [\"admin\"], \"password\": [\"admin\"] }" \
           "$scenario.yml" \
           --output "$scenario.json" \
-          > "tmp/$version-$triplestore-$scenario.txt"
+          > "tmp/$version-$dataset-$scenario.txt"
         wait
       done
     done
